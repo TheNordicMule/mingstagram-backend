c970814cbcd95c4a8e8254091bef1caf2fcc74e2
adolhunter <mingshiwang123@gmail.com>
2020-06-29 16:13:07 -0500

    sample usercontroller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const userController = express.Router();
+const {User} = require('../database/model/index')
 
 userController.get("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
+  const newUser = new User({
+    email: "demo@gmail.com",
+    username: "hello",
   });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    }
+  })
+  res.send('success!');
 });
 
 module.exports = userController

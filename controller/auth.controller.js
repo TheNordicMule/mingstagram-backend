50526a3b5b840bfab9e6bf9803b1757672a33d0b
adolhunter <mingshiwang123@gmail.com>
2020-07-02 15:57:34 -0500

    integrate UserContext

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require("express");
 const authController = express.Router();
 const User = require("../database/model/user.model");
@@ -6,14 +7,19 @@ const asyncHandler = require("express-async-handler");
 authController.post(
   "/login",
   asyncHandler(async (req, res, next) => {
-    console.log(req.body)
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user) return res.status(400).json({success: false, message: "No user found"});
+    let user = await User.findOne({ username });
+    if (!user)
+      return res.status(400).json({ success: false, message: "No user found" });
     const correct = await user.checkPassword(password);
-    if (!correct) return next(JSON.stringify({ message: "wrong password" }));
+    if (!correct)
+      return res
+        .status(422)
+        .json({ success: false, message: "Wrong password!" });
     const token = user.getJwtToken();
-    res.status(200).json({ success: true, token });
+    user = await User.findOne({ username }).select("-password");
+    console.log(user);
+    res.status(200).json({ success: true, token, user });
   })
 );
 
@@ -21,7 +27,10 @@ authController.post(
   "/signup",
   asyncHandler(async (req, res, next) => {
     const { username, password, email, fullname } = req.body;
-    if (await User.count({username}) != 0 ||await User.count({email}) != 0) {
+    if (
+      (await User.count({ username })) != 0 ||
+      (await User.count({ email })) != 0
+    ) {
       return next(JSON.stringify({ message: "Such accounts already exists" }));
     }
     const newUser = await User.create({
